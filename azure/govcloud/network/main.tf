dd0a47aa1baf5ea1217a64530015e13500b5912b
C Turner <christopher.turner.a@gmail.com>
2025-01-16 12:26:22 -0500

    fixing data sources and vnet name

--- azure/govcloud/network/main.tf
+++ azure/govcloud/network/main.tf
@@ -1,34 +1,34 @@
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
   address_space       = var.vnet_address_space
-  location            = azurerm_resource_group.network.location
+  location            = data.terraform_remote_state.iam.outputs.resource_group_location
   resource_group_name = data.terraform_remote_state.iam.outputs.resource_group_name
 }
 
 resource "azurerm_subnet" "aks" {
   name                 = var.aks_subnet_name
-  resource_group_name  = azurerm_resource_group.network.name
-  virtual_network_name = data.terraform_remote_state.iam.outputs.resource_group_name
+  resource_group_name  = data.terraform_remote_state.iam.outputs.resource_group_name
+  virtual_network_name = var.vnet_name
   address_prefixes     = var.aks_subnet_address_space
 }
 
 resource "azurerm_subnet" "virtual_node" {
   name                 = var.virtual_node_subnet_name
-  resource_group_name  = azurerm_resource_group.network.name
+  resource_group_name  = data.terraform_remote_state.iam.outputs.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = var.virtual_node_subnet_address_space
 }
 
 resource "azurerm_lb" "lb" {
   name                = var.lb_name
-  location            = azurerm_resource_group.network.location
+  location            = data.terraform_remote_state.iam.outputs.resource_group_location
   resource_group_name = data.terraform_remote_state.iam.outputs.resource_group_name
   sku                 = "Standard"
 }
 
 resource "azurerm_public_ip" "lb_ip" {
   name                = var.public_ip_name
-  location            = azurerm_resource_group.network.location
+  location            = data.terraform_remote_state.iam.outputs.resource_group_location
   resource_group_name = data.terraform_remote_state.iam.outputs.resource_group_name
   allocation_method   = "Static"
 }
